4156f99fa861625cd0ae5e9f1e0da226a818f0b0
Pedro <pedrohpinho1977@gmail.com>
2024-11-05 02:13:34 -0300

    Atualizei o totalPrice para que altere automaticamente, adicionei para fazer a modificação de status de um pedido, ver o status pelo id do pedido e ver qual id do prato está em determinado status

--- src/main/java/com/ibeus/Comanda/Digital/repository/OrderRepository.java
+++ src/main/java/com/ibeus/Comanda/Digital/repository/OrderRepository.java
@@ -2,6 +2,12 @@ package com.ibeus.Comanda.Digital.repository;
 
 import com.ibeus.Comanda.Digital.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long>{
+    List<Order> findByStatus(String status);
 }
--- src/main/java/com/ibeus/Comanda/Digital/service/OrderService.java
+++ src/main/java/com/ibeus/Comanda/Digital/service/OrderService.java
@@ -1,13 +1,16 @@
 package com.ibeus.Comanda.Digital.service;
 
 import com.ibeus.Comanda.Digital.model.Order;
+import com.ibeus.Comanda.Digital.model.OrderStatusHistory;
 import com.ibeus.Comanda.Digital.repository.OrderRepository;
+import com.ibeus.Comanda.Digital.repository.OrderStatusHistoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -15,6 +18,12 @@ public class OrderService {
 
     @Autowired
     private OrderRepository orderRepository;
+
+    @Autowired
+    private OrderStatusHistoryService orderStatusHistoryService;
+
+    @Autowired
+    private OrderStatusHistoryRepository orderStatusHistoryRepository;
 
     public Order findById(Long id) {
         return orderRepository.findById(id)
@@ -33,15 +42,48 @@ public class OrderService {
     @Transactional
     public Order updateOrder(Long id, Order updatedOrder) {
         return orderRepository.findById(id).map(order -> {
+            String previousStatus = order.getStatus();
             order.setStatus(updatedOrder.getStatus());
             order.setTotalPrice(updatedOrder.getTotalPrice());
+            Order savedOrder = orderRepository.save(order);
+
+            if (!previousStatus.equals(updatedOrder.getStatus())) {
+                orderStatusHistoryService.addStatusHistory(savedOrder, updatedOrder.getStatus());
+            }
+
+            return savedOrder;
+        }).orElseThrow(() -> new RuntimeException("Pedido não encontrado: " + id));
+    }
+    @Transactional
+    public void deleteOrder(Long orderId) {
+        orderStatusHistoryService.deleteByOrderId(orderId);
+        orderRepository.deleteById(orderId);
+    }
+
+    public List<Order> findOrdersByStatus (String status) {
+        return orderRepository.findByStatus(status);
+    }
+
+
+    public String findOrderStatusById (Long id) {
+        return orderRepository.findById(id)
+                .map(Order::getStatus)
+                .orElseThrow(() -> new RuntimeException("Pedido não encontrado com o ID: " + id));
+    }
+
+    public Order updateOrderStatus(Long id, String newStatus) {
+        return orderRepository.findById(id).map(order -> {
+            order.setStatus(newStatus);
+
+            OrderStatusHistory statusHistory = new OrderStatusHistory();
+            statusHistory.setOrder(order);
+            statusHistory.setStatus(newStatus);
+            statusHistory.setTimestamp(LocalDateTime.now());
+
+            orderStatusHistoryRepository.save(statusHistory);
+
             return orderRepository.save(order);
         }).orElseThrow(() -> new RuntimeException("Pedido não encontrado: " + id));
     }
-
-    public void deleteOrder(Long id) {
-        orderRepository.deleteById(id);
-    }
-
 
 }
