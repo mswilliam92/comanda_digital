0311ebe84c6a19607c20d0a853e1939dd2b29677
Pedro <pedrohpinho1977@gmail.com>
2024-11-22 23:58:02 -0300

    refatoracao da model Order

--- src/main/java/com/ibeus/Comanda/Digital/service/OrderService.java
+++ src/main/java/com/ibeus/Comanda/Digital/service/OrderService.java
@@ -1,6 +1,8 @@
 package com.ibeus.Comanda.Digital.service;
 
+import com.ibeus.Comanda.Digital.model.Dish;
 import com.ibeus.Comanda.Digital.model.Order;
+import com.ibeus.Comanda.Digital.repository.DishRepository;
 import com.ibeus.Comanda.Digital.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,23 @@ public class OrderService {
 
     @Autowired
     private OrderRepository orderRepository;
+
+    @Autowired
+    private DishRepository dishRepository;
+
+    public Order createOrder(Order order){
+//        for (Dish dish: order.getProducts()){
+//            dishRepository.save(dish);
+//        }
+//        dishRepository.save(order.getProducts().get(0));
+        Dish dish = new Dish();
+        dish.setPrice(10.0);
+        dish.setName("BANANA");
+        dish.setDescription("BANANA GRANDE");
+
+        dishRepository.save(dish);
+        return orderRepository.save(order);
+    }
 }
 
 
